30afbcff18dea26af8faf6ced37ca4118d4af4e5
Tim Arney <tim@line37.com>
2021-04-11 18:53:54 -0400

    pull in from main

--- consumer/src/App.jsx
+++ consumer/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import {Scheduler} from "timarney-component/slider";
+import {Scheduler} from "timarney-component/main";
 
 import "./index.css";
 
